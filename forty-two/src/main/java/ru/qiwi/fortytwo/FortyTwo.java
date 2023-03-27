6028559095dfdc41b6c9f393c72d2a60ee92d484
s.spirkin <s.spirkin@qiwi.com>
2023-03-27 20:40:57 +0300

    hw1/forty-two

--- forty-two/src/main/java/ru/qiwi/fortytwo/FortyTwo.java
+++ forty-two/src/main/java/ru/qiwi/fortytwo/FortyTwo.java
@@ -1,5 +1,42 @@
 package ru.qiwi.fortytwo;
 
-public class FortyTwo {
-    // TODO
+public class FortyTwo extends Number implements Comparable<FortyTwo>, TheUltimateQuestionOfLifeTheUniverseAndEverything {
+
+    public final int value = 42;
+
+    @Override
+    public int intValue() {
+        return 42;
+    }
+
+    @Override
+    public long longValue() {
+        return 42L;
+    }
+
+    @Override
+    public float floatValue() {
+        return 42f;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 42d;
+    }
+
+    @Override
+    public int compareTo(FortyTwo o) {
+        if (this.value > o.value) {
+            return 1;
+        } else if (this.value < o.value) {
+            return -1;
+        }
+        return 0;
+    }
+
+
+    @Override
+    public void getAnswer() {
+        System.out.print(this.value);
+    }
 }
